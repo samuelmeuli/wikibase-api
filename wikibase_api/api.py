08f36729f17d9a9d4d58f9e38551eb5c25ccf3f5
Samuel Meuli <me@samuelmeuli.com>
2018-09-10 07:52:24 +0200

    Add tests, docstrings

--- wikibase_api/api.py
+++ wikibase_api/api.py
@@ -3,7 +3,17 @@ from requests_oauthlib import OAuth1
 
 
 class Api:
+    """
+    Class for connecting to the Wikibase API
+    """
+
     def __init__(self, config):
+        """
+        Create a session with the Wikibase API on class creation and set the
+        authorization header and params which remain the same for all future
+        requests
+        :param config: Configuration dict as defined in ./config.py
+        """
         self.config = config
         self.base_url = config["wikibaseInstance"] + "/api.php"
         self.session = requests.Session()
@@ -18,11 +28,25 @@ class Api:
         }
 
     def get(self, params):
+        """
+        Make a GET request to the Wikibase API
+        :param params: Query parameters to be encoded in the URL
+        :type params: dict
+        :return: Response object
+        :rtype dict
+        """
         r = self.session.get(url=self.base_url, params=params)
-        print(r.status_code)
-        print(r.text)
+        r.raise_for_status()  # Raise exception if status code indicates error
+        return r.json()
 
     def post(self, params, body):
+        """
+        Make a POST request to the Wikibase API
+        :param params: Query parameters to be encoded in the URL
+        :type params: dict
+        :return: Response object
+        :rtype dict
+        """
         r = self.session.post(url=self.base_url, params=params, data=body)
-        print(r.status_code)
-        print(r.text)
+        r.raise_for_status()  # Raise exception if status code indicates error
+        return r.json()
--- wikibase_api/wikibase.py
+++ wikibase_api/wikibase.py
@@ -5,17 +5,50 @@ from .config import get_config
 
 
 class Wikibase:
-    current_dir = os.path.dirname(os.path.realpath(__file__))
-    default_config_path = current_dir + "/../config/config.json"
+    """
+    Wrapper library for the Wikibase API
+    """
 
-    def __init__(self, config_path=default_config_path):
+    def __init__(self, config_path=None):
+        """
+        Parse the config object and start a request session with the Wikidata
+        API
+        :param config_path: Path to the configuration file (in case the user
+        does not want to save it at ../config/config.json)
+        :type config_path: str
+        """
+        # Use default config path if param is not provided
+        if config_path is None:
+            current_dir = os.path.dirname(os.path.realpath(__file__))
+            config_path = current_dir + "/../config/config.json"
+
+        # Load options from config file
         config = get_config(config_path)
+
+        # Set up API session
         self.api = Api(config)
 
-    def get_entities(self, ids):
-        ids_encoded = "|".join(ids)
+    def get_entity(self, item_id):
+        """
+        Get the data of one Wikibase entity
+        :param item_id: Item identifier
+        :type item_id: str (e.g. "Q1")
+        :return: Response
+        :rtype dict
+        """
+        return self.get_entities([item_id])
+
+    def get_entities(self, item_ids):
+        """
+        Get the data of multiple Wikibase entities
+        :param item_ids: Item identifiers
+        :type item_ids: list(str) (e.g. ["Q1", "Q2"])
+        :return: Response
+        :rtype dict
+        """
+        ids_encoded = "|".join(item_ids)
         params = {
             "action": "wbgetentities",
             "ids": ids_encoded
         }
-        self.api.get(params)
+        return self.api.get(params)
