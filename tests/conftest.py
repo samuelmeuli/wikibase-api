3ab65ceac40ca4627e1f8db85498e047e9fa68b0
Samuel Meuli <me@samuelmeuli.com>
2018-10-10 14:47:54 +0200

    Cancel tests if run on live Wikidata instance

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,4 +11,8 @@ def wikibase():
     except Exception as e:
         pytest.exit("Could not create Wikibase object: " + str(e))
 
+    # Prevent tests from being run on live Wikidata instance
+    if wb.config["apiUrl"] == "https://www.wikidata.org/w/api.php":
+        pytest.exit("Tests must not be run on the live Wikidata instance!")
+
     return wb
