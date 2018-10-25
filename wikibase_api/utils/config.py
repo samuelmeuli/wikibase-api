22511df9f431637ab5b7d220f0648e6769c1e88e
Samuel Meuli <me@samuelmeuli.com>
2018-10-25 17:17:53 +0200

    Allow empty dict for credentials

--- wikibase_api/utils/config.py
+++ wikibase_api/utils/config.py
@@ -7,17 +7,17 @@ def verify_auth_info(oauth_credentials, login_credentials):
     Verify that the provided authentication credentials are of the correct form (i.e. exactly one
     authentication method is specified and the corresponding parameter contains the correct keys)
     """
-    if oauth_credentials is None and login_credentials is None:
+    if not oauth_credentials and not login_credentials:
         raise KeyError(
             'Authentication information missing: Either the "oauth_credentials" or the '
             '"login_credentials" parameter must be provided to the Wikibase class'
         )
-    if oauth_credentials is not None and login_credentials is not None:
+    if oauth_credentials and login_credentials:
         raise KeyError(
             'Authentication information conflict: Only one of the "oauth_credentials" and the '
             '"login_credentials" parameters must be provided to the Wikibase class'
         )
-    if oauth_credentials is not None:
+    if oauth_credentials:
         for key in required_oauth_entries:
             if key not in oauth_credentials:
                 raise KeyError(
